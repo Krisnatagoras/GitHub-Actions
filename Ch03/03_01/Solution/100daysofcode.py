2f23dc2eb1e7e27cfdf7102953d359609c8b6cb7
Krisnatagoras Araujo <50491859+Krisnatagoras@users.noreply.github.com>
2023-04-17 17:34:13 +0100

    testing syntax

--- Ch03/03_01/Solution/100daysofcode.py
+++ Ch03/03_01/Solution/100daysofcode.py
@@ -5,7 +5,8 @@ today = date.today()
 delta = today - start
 
 if (delta.days < 101):
-    print("Today is day {}".format(delta.days))
+print("Today is day {}".format(delta.days))
 else:
-    print('100 Days of Code sprint has ended')
-...
+print('100 Days of Code sprint has ended')
+
+addirheouherlneowh
